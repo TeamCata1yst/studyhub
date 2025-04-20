210dba020320401f23031fcdecd888aff11a144f
Naman Chandok <namanchandok1@gmail.com>
2025-04-20 19:37:53 +0530

    change, everything

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,20 @@
-import { NextRequest, NextResponse, NextMiddleware } from "next/server";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
 
-const PUBLIC_PATHS = [ '/login', '/forgot', '/signup', '/api/auth', '/handler' ];
-
-export const middleware: NextMiddleware = async (req: NextRequest) => {
-
-    const nexturl = req.nextUrl.clone()
-    
-    const url = new URL(req.url)?.pathname;
-
-    const token = cookies().get('token');
-    console.log(url);
-    // if (!token && !PUBLIC_PATHS.includes(url)) {
-    //     console.log('redirecting to login');
-    //     nexturl.pathname = '/login';
-    //     return NextResponse.redirect(nexturl.toString());
-    // }
-    // if (token && PUBLIC_PATHS.includes(url)) {
-    //     console.log('redirecting to home');
-    //     nexturl.pathname = '/';
-    //     return NextResponse.redirect(nexturl.toString());
-    // }
-    return NextResponse.next();
-};
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
 
 export const config = {
-    matcher: [
-        "/",
-        "/((?!_next|api|.*\\.).*)",
-        "/api/auth",
-    ]   
-}
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  compiler: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
-  },
-};
-
-export default nextConfig;
